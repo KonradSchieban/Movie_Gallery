3bcc5b3c078f763fb2a71e7d157b0d278193b61e
Konrad Schieban <konrad.schieban@gmail.com>
2016-07-14 19:57:34 +0200

    First Commit

---
+++ fresh_tomatoes.py
@@ -0,0 +1,56 @@
+import webbrowser
+import os
+import re
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+def render_str(template, **params):
+	# helper function which renders html from a template using jinja2
+
+	t = jinja_env.get_template(template)
+	return t.render(params)
+
+
+def create_movie_tiles_content(movies):
+	# The HTML content for this section of the page
+
+	content = ''
+	for movie in movies:
+		# Extract the youtube ID from the url
+		youtube_id_match = re.search(
+			r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+		youtube_id_match = youtube_id_match or re.search(
+			r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+		trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
+							  else None)
+
+		content += render_str("movie_tile_content.html",
+							  movie_title=movie.title,
+							  poster_image_url=movie.poster_image_url,
+							  trailer_youtube_id=trailer_youtube_id,
+							  movie_story_line=movie.story_line,
+							  imdb_link=movie.imdb_link,
+							  year=movie.year)
+
+	return content
+
+
+def open_movies_page(movies):
+
+	# Create or overwrite the output file
+	output_file = open('fresh_tomatoes.html', 'w')
+
+	# build HTML content from template
+	rendered_content = render_str("main_page.html", movie_tiles=create_movie_tiles_content(movies))
+
+	# Output the file
+	output_file.write(rendered_content)
+	output_file.close()
+
+	# open the output file in the browser (in a new tab, if possible)
+	url = os.path.abspath(output_file.name)
+	webbrowser.open('file://' + url, new=2)
+
